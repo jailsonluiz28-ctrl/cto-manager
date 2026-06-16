b212d26aa1c6490319ecd7b745053d61c0bf7c7b
Jailson Luiz Silva dos Santos Junior <jailsonluiz28@gmail.com>
2026-06-16 20:03:57 -0300

    Adicionado formulário de cadastro de CTO

--- ctos/forms.py
+++ ctos/forms.py
@@ -3,13 +3,18 @@ from .models import CTO
 
 
 class CTOForm(forms.ModelForm):
-
     class Meta:
         model = CTO
-
         fields = [
             'nome',
             'tipo',
             'rua',
             'ativa',
         ]
+
+        labels = {
+            'nome': 'Nome da CTO',
+            'tipo': 'Tipo',
+            'rua': 'Rua',
+            'ativa': 'Ativa',
+        }
